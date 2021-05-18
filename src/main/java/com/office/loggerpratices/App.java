ba0e12c7843db94bc7fd151cb96eaa76c2e8fafc
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-18 17:05:57 +0530

    ADDED:To create simpleLayout

--- src/main/java/com/office/loggerpratices/App.java
+++ src/main/java/com/office/loggerpratices/App.java
@@ -1,13 +1,22 @@
 package com.office.loggerpratices;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	static Logger logger = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Layout layout=new SimpleLayout();
+        Appender appender=new ConsoleAppender(layout);
+        
     }
 }
